a5aaef376940197e325e3abadabf5d8650ebc6db
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-10 18:09:33 -0500

    allow cross origin

--- app/app/settings.py
+++ app/app/settings.py
@@ -52,11 +52,13 @@ INSTALLED_APPS = [
     'user',
     'data',
     'simple_history',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -159,3 +161,8 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_HISTORY_HISTORY_CHANGE_REASON_USE_TEXT_FIELD = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
