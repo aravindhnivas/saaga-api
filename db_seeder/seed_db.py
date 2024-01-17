6339cee224654e45e7b6adb64c2626ae9d5583fa
aravindhnivas <aravindhnivas28@gmail.com>
2024-01-17 10:37:20 -0500

    log

--- db_seeder/seed_db.py
+++ db_seeder/seed_db.py
@@ -6,8 +6,8 @@ import argparse
 from loguru import logger
 import json
 
-logger.add("./logs/file_info_{time}.log", level="INFO")
-logger.add("./logs/file_error_{time}.log", level="ERROR")
+logger.add("./logs/file_info.log", level="INFO")
+logger.add("./logs/file_error.log", level="ERROR")
 load_dotenv()
 
 parser = argparse.ArgumentParser(description="Process some integers.")
@@ -43,19 +43,19 @@ def safe_post_request(endpoint, data, files=None):
     logger.info(f"Posting to {address=} with {data=}")
     headers = {"Authorization": TOKEN, "accept": "application/json"}
     res = requests.post(address, headers=headers, data=data, files=files)
-    
+
     if res.ok:
         logger.success(f"Successfully posted to {endpoint=}!\n\n")
-        
+
         # logger.info(f"Response: {res.text}")
         data = json.loads(res.text)
         logger.info(f"Linelist ID: {data['id']}")
-        
+
         if isinstance(data, dict):
-            return int(data['id'])
+            return int(data["id"])
         else:
             return None
-        
+
     else:
         logger.error(
             f"\nerror code: ({res.status_code}) posting to {endpoint=} with {data=}!\n "
@@ -71,16 +71,16 @@ linelist_ids = {}
 def post_linelist(linelist_list):
     global linelist_ids
     logger.info("Posting linelist data...")
-    
+
     for i, linelist in enumerate(linelist_list):
         linelist_id = safe_post_request(
             "/api/data/linelist/",
             data=linelist,
         )
-        
-        linelist_ids[i+1] = linelist_id
+
+        linelist_ids[i + 1] = linelist_id
         logger.info(f"{i} - Finished posting {linelist} ({linelist_ids[i+1]=})!")
-        
+
 
 def post_payload(payload_list):
     logger.info("Posting species data...")
@@ -92,11 +92,10 @@ def post_payload(payload_list):
             "/api/data/species/",
             data=payload["species"],
         )
-        
+
         if species_id is None:
             logger.error(f"Failed to post {payload['species']['iupac_name']}")
             continue
-        
 
         for meta in payload["species_metadata"]:
             # Posting species metadata
@@ -105,18 +104,20 @@ def post_payload(payload_list):
                 for x in meta
                 if x not in ["qpart_file", "reference", "meta_reference", "line"]
             }
-            
+
             meta_payload["species"] = species_id
             meta_payload["linelist"] = linelist_ids[meta_payload["linelist"]]
-            
+
             meta_id = safe_post_request(
                 "/api/data/species-metadata/",
                 data=meta_payload,
                 files={"qpart_file": meta["qpart_file"]},
             )
-            
+
             if meta_id is None:
-                logger.error(f"Failed to post metadata for {payload['species']['iupac_name']}")
+                logger.error(
+                    f"Failed to post metadata for {payload['species']['iupac_name']}"
+                )
                 continue
 
             for ref, meta_ref in zip(meta["reference"], meta["meta_reference"]):
@@ -126,14 +127,16 @@ def post_payload(payload_list):
                     data=ref_payload,
                     files={"bibtex": ref["bibtex"]},
                 )
-                
+
                 if ref_id is None:
-                    logger.error(f"Failed to post meta reference for {payload['species']['iupac_name']}")
+                    logger.error(
+                        f"Failed to post meta reference for {payload['species']['iupac_name']}"
+                    )
                     continue
-                
-                meta_ref['meta'] = meta_id
-                meta_ref['ref'] = ref_id
-                
+
+                meta_ref["meta"] = meta_id
+                meta_ref["ref"] = ref_id
+
                 safe_post_request(
                     "/api/data/meta-reference/",
                     data=meta_ref,
@@ -143,8 +146,8 @@ def post_payload(payload_list):
             line_payload = {
                 x: meta["line"][x] for x in meta["line"] if x not in ["cat_file"]
             }
-            
-            line_payload['meta'] = meta_id
+
+            line_payload["meta"] = meta_id
             safe_post_request(
                 "/api/data/line/",
                 data=line_payload,
