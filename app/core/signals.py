ff44b007f8e0d8430136c24b88546a756c699791
aravindhnivas <aravindhnivas28@gmail.com>
2024-02-14 17:59:16 -0500

    send email updates when user status changes

--- app/core/signals.py
+++ app/core/signals.py
@@ -1,8 +1,7 @@
 from django.conf import settings
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
-from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from .models import SpeciesMetadata, MetaReference
 import textwrap
@@ -12,8 +11,19 @@ from_email = settings.EMAIL_HOST_USER
 user_model = get_user_model()
 
 
+@receiver(pre_save, sender=get_user_model())
+def set_original_is_staff(sender, instance, **kwargs):
+    try:
+        original = sender.objects.get(pk=instance.pk)
+        instance._original_is_staff = original.is_staff
+        instance._original_is_superuser = original.is_superuser
+    except sender.DoesNotExist:
+        instance._original_is_staff = None
+
+
 @receiver(post_save, sender=user_model)
 def send_update_notification(sender, instance, created, **kwargs):
+    message = ""
 
     if created:
         subject = f"[SaagaDb] {instance.name}: Your account has been created"
@@ -27,6 +37,31 @@ def send_update_notification(sender, instance, created, **kwargs):
             The approver will review and approve your data uploads: species- and reference- metadata.
         """
         ).strip()
+    else:
+        if instance.is_staff and instance.is_staff != instance._original_is_staff:
+            subject = (
+                f"[SaagaDb] {instance.name}: Your account has been promoted to admin"
+            )
+            message = textwrap.dedent(
+                f"""
+                Your account has been promoted to admin.
+                You can now approve data uploads: species- and reference- metadata.
+            """
+            ).strip()
+
+        if (
+            instance.is_superuser
+            and instance.is_superuser != instance._original_is_superuser
+        ):
+            subject = f"[SaagaDb] {instance.name}: Your account has been promoted to superuser"
+            message = textwrap.dedent(
+                f"""
+                Your account has been promoted to superuser.
+                You can now approve data uploads: species- and reference- metadata.
+            """
+            ).strip()
+
+    if message:
         recipient_list = [instance.email]
         send_mail(subject, message, from_email, recipient_list)
         print(f"Email sent successfully to {instance.email}")
