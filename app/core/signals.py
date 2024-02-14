65717c6467be058062aef2c09ba011a38eb95c2e
aravindhnivas <aravindhnivas28@gmail.com>
2024-02-14 11:44:26 -0500

    send email for approval

--- app/core/signals.py
+++ app/core/signals.py
@@ -4,29 +4,51 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-
 from .models import (
     SpeciesMetadata,
     MetaReference,
-)  # Replace with your actual model name
+)
+
+from_email = settings.EMAIL_HOST_USER
+
+
+@receiver(post_save, sender=MetaReference)
+def send_update_notification(sender, instance, created, **kwargs):
+    user = instance.uploaded_by
+    if created and not instance.approved and user.approver:
+        subject = "[SaagaDb] New reference metadata uploaded for approval"
+        message = f"New metadata has been uploaded by {user.name} ({user.email}). Please review and approve it."
+        recipient_list = [user.approver.email]
+        send_mail(subject, message, from_email, recipient_list)
+        # print(f"Email sent successfully to {user.approver.email}")
+
+    if not created and instance.approved:
+        # print("Metadata updated and approved")
+        subject = "[SaagaDb] Reference metadata approved"
+        message = f"Reference metadata ({instance.ref_url}) approved by {user.approver.name} ({user.approver.email})."
+        recipient_list = [user.email]
+        send_mail(subject, message, from_email, recipient_list)
+        # print(f"Email sent successfully to {user.email}")
 
 
 @receiver(post_save, sender=SpeciesMetadata)
 def send_update_notification(sender, instance, created, **kwargs):
-    # print(f"Your model instance ({instance.id}) has been updated.")
-    if not created:  # Only send email if the instance was updated
-        subject = "Update Notification for SpeciesMetadata"
-        message = f"Your model instance ({instance.id}) has been updated."  # Customize your message
-        from_email = settings.EMAIL_HOST_USER  # Your project's email
-        # recipient_list = [instance.user.email]  # Assuming your model has a 'user' field
-        recipient_list = ["nivasm@mit.edu"]
+    user = instance.uploaded_by
+    species_name = instance.species.iupac_name
+    if created and not instance.approved and user.approver:
+        subject = "[SaagaDb] New species metadata uploaded for approval"
+        message = f"New metadata for {species_name} has been uploaded by {user.name} ({user.email}). Please review and approve it."
+        recipient_list = [user.approver.email]
         send_mail(subject, message, from_email, recipient_list)
-        print("Email sent successfully")
+        # print(f"Email sent successfully to {user.approver.email}")
 
-
-@receiver(user_logged_in)
-def log_user_login(sender, user, request, **kwargs):
-    print(f"User {user} logged in")
+    if not created and instance.approved:
+        # print("Metadata updated and approved")
+        subject = "[SaagaDb] Species metadata approved for " + species_name
+        message = f"Species metadata for {species_name} approved by {user.approver.name} ({user.approver.email})."
+        recipient_list = [user.email]
+        send_mail(subject, message, from_email, recipient_list)
+        # print(f"Email sent successfully to {user.email}")
 
 
 def send_email_test():
