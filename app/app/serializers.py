c2be4ec44e44b08ca0b3a0383691da4ef3e5c9b3
aravindhnivas <aravindhnivas28@gmail.com>
2024-03-19 14:35:59 -0400

    include user info on JWT token

--- app/app/serializers.py
+++ app/app/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.forms.models import model_to_dict
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
 
-        # Add custom claims
-        token['name'] = user.name
-        
+        token["user_approvers"] = list(
+            user.approver.all().values_list("name", flat=True)
+        )
+
+        token["user"] = model_to_dict(
+            user,
+            fields=[
+                "id",
+                "name",
+                "email",
+                "organization",
+                "is_staff",
+                "is_superuser",
+                "is_active",
+            ],
+        )
+
         return token
