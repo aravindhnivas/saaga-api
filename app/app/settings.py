349adccf7354541831feba587980c46f3d84e1b6
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-04-08 23:50:40 -0400

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -185,7 +185,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-FRONTEND_URL = "http://localhost:5173" if DEBUG else "http://herzberg.mit.edu/saagadb"
+FRONTEND_URL = (
+    "http://localhost:5173/saagadb" if DEBUG else "http://herzberg.mit.edu/saagadb"
+)
 # PORT 5173 is the frontend port (sveltekit project - saagadb-ui)
 # This is only valid when the frontend is running in development mode on a user computer.
 # So this is only for development purposes.
